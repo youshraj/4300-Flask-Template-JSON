4ddb83029b5b12e75669fd7e7ef2dfa0ecefe72f
Amy Sharin <amysharin1@gmail.com>
2024-03-19 23:38:37 -0400

    get image based on name also

--- backend/wiki_scrape.py
+++ backend/wiki_scrape.py
@@ -1,6 +1,8 @@
 import wikipediaapi
+import requests
 
 celeb_to_summary_dict = {}
+celeb_to_imgs_dict = {}
 
 def get_celeb_info(celeb_name):
     # Initialize Wikipedia API
@@ -16,15 +18,34 @@ def get_celeb_info(celeb_name):
         return summary
     else:
         return None
+    
+def get_celeb_url(celeb_name):
+    url = f"https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages&titles={celeb_name}&pithumbsize=300"
+    response = requests.get(url)
+
+    data = response.json()
+    pages = data.get('query', {}).get('pages', {})
+    for page_id, page_info in pages.items():
+        thumbnail_info = page_info.get('thumbnail')
+        if thumbnail_info:
+            return thumbnail_info.get('source')
+
+    return None
 
 
 celeb_names = ["Tom Hanks", "Ariana Grande"] #expand to 1000 names
 for celeb_name in celeb_names:
     summary = get_celeb_info(celeb_name)
+    url = get_celeb_url(celeb_name)
     if summary:
         celeb_to_summary_dict[celeb_name] = summary
         # print(f"{celeb_name} Summary:")
         # print(summary)
     else:
-        print(f"No information for {celeb_name} on Wikipedia.")
-# print(celeb_to_summary_dict)
+        print(f"No summary for {celeb_name} on Wikipedia.")
+    if url:
+        celeb_to_imgs_dict[celeb_name] = url
+    else:
+        print(f"No image for {celeb_name} on Wikipedia")
+#print(celeb_to_summary_dict)
+#print(celeb_to_imgs_dict)
